1f932f7eec8e40dcaa45b7838cdab5771f82406b
Daniel <Timetrapper@users.noreply.github.com>
2018-04-04 10:37:31 -0700

    Updating and Proofreading
    - Proofread Technical Requirements

--- scripts/techInfo.js
+++ scripts/techInfo.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    $('#tree').tree({
-      uiLibrary: 'bootstrap4',
-      dataSource: [
-        { text: 'Software requirements and recommendations', children: [ { text: 'Opeartion System', children: [ { text: 'WIN10' }, { text: 'IOS' } ] }, { text: 'Visual Studio' },  { text: 'Android Studio' } ] },
-        { text: 'Computer requirements and recommendations', children: [ { text: 'Harddrive 1TB' },  { text: '8GB RAM' },  { text: 'Easy to Carry' } ] },
-        { text: 'Web Services requirements', children: [ { text: 'AWS' },  { text: 'GitHub' },  { text: 'Mlab' }, { text: 'Azure' },{ text: 'HeroKu' }] },
-        { text: 'Dev environment setup recommendations or requirements', children: [ { text: 'Visual Studio Code' },  { text: 'Visual Studio 2017' },  { text: 'GitHub Desktop' } ] }
-      ],
-      width: 500
-      
-    });
+  $('#tree').tree({
+    uiLibrary: 'bootstrap4',
+    dataSource: [
+      { text: 'Software Requirements and Recommendations', children: [ { text: 'Operating System', children: [ { text: 'Windows 10' }, { text: 'IOS' } ] }, { text: 'Visual Studio' },  { text: 'Android Studio' } ] },
+      { text: 'Computer Requirements and Recommendations', children: [ { text: 'Hard Drive 1TB' },  { text: '8GB RAM' },  { text: 'Easy to Carry' } ] },
+      { text: 'Web Services Requirements', children: [ { text: 'AWS' },  { text: 'GitHub' },  { text: 'Mlab' }, { text: 'Azure' },{ text: 'HeroKu' }] },
+      { text: 'Developer Environment Setup Recommendations / Requirements', children: [ { text: 'Visual Studio Code' },  { text: 'Visual Studio 2017' },  { text: 'GitHub Desktop' } ] }
+    ],
+    width: 500
+    
+  });
 });
